29a7dd45c17d6baa11a2979d6775faa53182e901
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-08-22 05:02:50 +0530

    update

--- src/utils/gptconfig.js
+++ src/utils/gptconfig.js
@@ -2,6 +2,6 @@ import OpenAI from 'openai';
 
 
 export const openai = new OpenAI({
-  apiKey:import.meta.env.VITE_GPT_KEY,
+  apiKey:GPT_KEY,
   dangerouslyAllowBrowser: true  // This is the default and can be omitted
 });
